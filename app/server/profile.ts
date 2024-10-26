ee4c7fddeefef5d865fc2f343e66cb1494387572
kosei28 <contact@kosei28.com>
2024-10-27 04:47:19 +0900

    feat: プロフィール更新のバックエンド実装

--- app/server/profile.ts
+++ app/server/profile.ts
@@ -1,8 +1,8 @@
 import { createServerFn } from '@tanstack/start';
 import { db } from '../db/client';
-import { getImagePublicUrl, uploadImage } from '../repos/image';
-import { createProfile, getProfile } from '../repos/profile';
-import { createProfileSchema, getProfileSchema } from '../schemas/profile';
+import { deleteImages, getImagePublicUrl, uploadImage } from '../repos/image';
+import { createProfile, getProfile, updateProfile } from '../repos/profile';
+import { createProfileSchema, getProfileSchema, updateProfileSchema } from '../schemas/profile';
 import { serverZodValidator } from '../utils/server';
 import { getSupabaseServerClient } from '../utils/supabase';
 import { getAuthUserFn } from './auth';
@@ -44,5 +44,58 @@ export const createProfileFn = createServerFn(
 			pictureUrls,
 		});
 		return profile;
+	}),
+);
+
+export const updateProfileFn = createServerFn(
+	'POST',
+	serverZodValidator(updateProfileSchema, async (value) => {
+		const user = await getAuthUserFn();
+		if (user === null) {
+			throw new Error('Unauthorized');
+		}
+
+		const supabase = getSupabaseServerClient();
+		const newPicturePaths = await Promise.all(
+			value.newPictures.map(async (picture) => {
+				const { path } = await uploadImage(supabase, user.id, picture);
+				return path;
+			}),
+		);
+		const newPictureUrls = await Promise.all(
+			newPicturePaths.map(async (path) => {
+				const url = await getImagePublicUrl(supabase, path);
+				return url;
+			}),
+		);
+
+		const { profile, deletePicturePaths } = await db.transaction(async (tx) => {
+			const oldProfile = await getProfile(tx, { userId: user.id });
+			if (oldProfile === undefined) {
+				throw new Error('Profile not found');
+			}
+
+			const picturePaths = [
+				...oldProfile.picturePaths!.filter((_, i) => !value.deletePictureIndices.includes(i)),
+				...newPicturePaths,
+			];
+			const pictureUrls = [
+				...oldProfile.pictureUrls!.filter((_, i) => !value.deletePictureIndices.includes(i)),
+				...newPictureUrls,
+			];
+			const deletePicturePaths = oldProfile.picturePaths!.filter((_, i) => value.deletePictureIndices.includes(i));
+
+			const profile = await updateProfile(tx, user.id, {
+				...value,
+				picturePaths,
+				pictureUrls,
+			});
+
+			return { profile: profile!, deletePicturePaths };
+		});
+
+		await deleteImages(supabase, deletePicturePaths);
+
+		return { profile };
 	}),
 );
--- app/types/profile.ts
+++ app/types/profile.ts
@@ -1,4 +1,6 @@
 import type { InferInsertModel } from 'drizzle-orm';
 import type { profileTable } from '../db/schema';
+import type { AllOptional } from './utils';
 
 export type CreateProfileValue = InferInsertModel<typeof profileTable>;
+export type UpdateProfileValue = AllOptional<CreateProfileValue>;
