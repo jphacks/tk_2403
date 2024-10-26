3a54ab0f66b14edb7adfd50a11d9b2f08b019d17
SomaTakata <somatakata@gmail.com>
2024-10-26 12:18:17 +0900

    feat: 選択画面の作成

--- app/routes/_authed/index.tsx
+++ app/routes/_authed/index.tsx
@@ -1,24 +1,42 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { useServerFn } from '@tanstack/start';
-import { logoutFn } from '../../server/auth';
+import { FaHouse } from 'react-icons/fa6';
+import { GoPersonFill } from 'react-icons/go';
+import { css } from '../../../styled-system/css';
+import Card from '../../components/selectPage/Card';
+import { containerStyle } from '../../styles/layout';
 
 export const Route = createFileRoute('/_authed/')({
 	component: Home,
 });
 
 function Home() {
-	const logout = useServerFn(logoutFn);
-
 	return (
-		<main>
-			<button
-				type="button"
-				onClick={async () => {
-					await logout();
-				}}
+		<div className={containerStyle()}>
+			<h2
+				className={css({
+					mb: '24',
+					color: 'text.muted',
+					textAlign: 'center',
+					fontSize: '[24px]',
+					fontWeight: 'black',
+				})}
 			>
-				ログアウト
-			</button>
-		</main>
+				どちらで進めますか？
+			</h2>
+			<div
+				className={css({
+					spaceY: '5',
+				})}
+			>
+				<Card color="primary" to="/guest/register">
+					<GoPersonFill size={40} />
+					退避先を探す
+				</Card>
+				<Card color="safe" to="/host/register">
+					<FaHouse size={40} />
+					退避先を提供する
+				</Card>
+			</div>
+		</div>
 	);
 }
