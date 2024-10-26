6218216881c139c117eacd5e13e0c576cb581156
SomaTakata <somatakata@gmail.com>
2024-10-26 14:32:13 +0900

    feat: guest側のプロフィール入力画面の作成

--- app/routes/_authed/guest/resister.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_authed/guest/resister')({
-  component: () => <div>Hello /_authed/guest/resister!</div>,
-})
