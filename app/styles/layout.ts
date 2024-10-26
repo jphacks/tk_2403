e32986b66e3dafbc986913202e6718bcf57f0d03
balckowl <shiggyjr1492@gmail.com>
2024-10-26 22:42:35 +0900

    containerの種類を追加

--- app/styles/layout.ts
+++ app/styles/layout.ts
@@ -7,7 +7,30 @@ export const containerStyle = cva({
 		minH: '[dvh]',
 		mx: 'auto',
 		py: '8',
-		px: '4',
+		px: '[15px]',
 		pb: '24',
+	},
+});
+
+export const profileContainerStyle = cva({
+	base: {
+		display: 'flex',
+		flex: '1',
+		flexDirection: 'column',
+		w: '[600px]',
+		maxW: 'full',
+		minHeight: '[calc(100vh - 80px - 50px)]',
+		mx: 'auto',
+	},
+});
+
+export const layoutStyle = cva({
+	base: {
+		w: '[600px]',
+		maxW: 'full',
+		minHeight: '[calc(100vh - 80px - 50px)]',
+		mx: 'auto',
+		py: '4',
+		px: '[15px]',
 	},
 });
